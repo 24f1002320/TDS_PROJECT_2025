0e96d932ac08b6e571722c3991d014a26722e82d
Irfan <24f1002320@ds.study.iitm.ac.in>
2025-10-17 03:37:27 +0530

    final

--- Project1/Student/fibonacci_code.py
+++
@@ -1,7 +0,0 @@
-Sure! Here are five cities in India:
-
-1. Mumbai
-2. Delhi
-3. Bangalore
-4. Chennai
-5. Kolkata
--- Project1/Student/generated_code.py
+++
@@ -1,8 +0,0 @@
-
-def fibonacci_sequence(n):
-    sequence = []
-    a, b = 0, 1
-    while a <= n:
-        sequence.append(a)
-        a, b = b, a + b
-    return sequence
